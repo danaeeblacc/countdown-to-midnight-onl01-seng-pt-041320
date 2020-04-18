df51ddcb03b76251be59a39804ff56b94122a141
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-18 01:12:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(int)
   
   while int > 0
     puts "#{int} SECONDS(S)!"
-    int -= 1
+    int += 1
   end
   "HAPPY NEW YEAR!"
 end
@@ -12,7 +12,7 @@ end
 def countdown_with_sleep(int)
   while int > 0
     puts "#{int} SECOND(S)!"
-    int += 1
+    int -= 1
     sleep 1
   end
   "HAPPY NEW YEAR!"
